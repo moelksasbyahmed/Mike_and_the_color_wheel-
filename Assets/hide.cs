d844513a4c70874d9174c4de5dcf7e00e537d574
ETMS <123107746+EveryTGames@users.noreply.github.com>
2025-11-28 18:11:51 +0200

    fixed something
    made the touch controls be hidden  in not touch screens

---
+++ Assets/hide.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hide : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!Application.isMobilePlatform || !Input.touchSupported)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
